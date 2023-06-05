ef2ee1c972a966038e35eefd2351cab860366450
ifazalam1 <ifaz.alam1@gmail.com>
2023-06-05 18:19:32 +0600

    invoice changes

--- resources/views/admin/Backend/Sales/manage_sales.blade.php
+++ resources/views/admin/Backend/Sales/manage_sales.blade.php
@@ -18,6 +18,7 @@
 											<th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Date</th>
 											<th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Customer</th>
 											<th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Invoice No.</th>
+											<th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Previous Invoice</th>
 											<th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Grand Total</th>
 											<th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Paid</th>
 											<th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Sold By</th>
@@ -35,6 +36,7 @@
         <td><p class="mb-0 text-sm">{{ $item->sale_date }}</p></td>
 		<td class="text-sm font-weight-bold mb-0">{{ $item->customer->customer_name }}</td>
 		<td class="text-sm font-weight-bold mb-0">{{ $item->invoice }}</td>
+		<td class="text-sm font-weight-bold mb-0">{{ $item->pInvoice }}</td>
 		<td class="text-sm font-weight-bold mb-0">TK {{ $item->grand_total }} </td>
 		<td class="text-sm font-weight-bold mb-0">TK {{ $item->p_paid_amount }} </td>
 		<td><h6 class="badge badge-sm bg-gradient-success"> {{ $item->user->name }}</h6></td>
