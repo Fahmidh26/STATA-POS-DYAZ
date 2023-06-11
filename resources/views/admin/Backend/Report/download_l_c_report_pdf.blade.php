ba7722a425a2a9733c1eed92b5b6010a0b1fa83e
ifazalam1 <ifaz.alam1@gmail.com>
2023-06-11 12:13:10 +0600

    lc download ifaz

--- resources/views/admin/Backend/Report/download_l_c_report_pdf.blade.php
+++ resources/views/admin/Backend/Report/download_l_c_report_pdf.blade.php
@@ -1,4 +1,4 @@
-<!doctype html>
+{{-- <!doctype html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
@@ -20,11 +20,9 @@
     th, td{
     padding: 0px 30px 0px 30px;
     }
-      /* Regular styles here */
-      
-      /* Styles for print output */
+
       @media print {
-          /* Define styles here */
+        
           .print-button {
               display: none;
           }
@@ -44,9 +42,7 @@
           <th class="text-center w-5">Date</th>
           <th class="text-center w-5">L/C no.</th>
           <th class="text-center w-5">Supplier</th>
-          <th class="text-center w-5">Product</th>
-          <th class="text-center w-5">Qty</th>
-          <th class="text-center w-5">Grand Total</th>                  
+          <th class="text-center w-5">Amount</th>                  
         </tr>
       </thead>
       <tbody>
@@ -61,16 +57,12 @@
 						<td>{{ $purchase->purchase_date }}</td>
             <td>{{ $purchase->chalan_no }}</td>
 						<td>{{ $purchase->supplier->supplier_name }}</td>
-						<td>{{ $pitem->product->product_name }}</td>
-						<td>{{ $pitem->qty }}</td>
 						<td>{{ $purchase->grand_total }}</td>
 						<td style="display:none;">{{$amount += $purchase->grand_total}}</td>
 					</tr>
           @endforeach
         @endforeach
         <tr>
-          <td></td>
-          <td></td>
           <td></td>
           <td></td>
           <td></td>
@@ -82,4 +74,133 @@
     </table>
 
   </body>
-</html>
+</html> --}}
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<title>L/C Report</title>
+
+<style type="text/css">
+    * {
+        font-family: Verdana, Arial, sans-serif;
+    }
+    table{
+        font-size: x-small;
+    }
+    tfoot tr td{
+        font-weight: bold;
+        font-size: x-small;
+    }
+    .gray {
+        background-color: lightgray
+    }
+    .font{
+      font-size: 13px;
+    }
+    .authority {
+        /*text-align: center;*/
+        float: right
+    }
+
+    .authority1 {
+        /*text-align: center;*/
+        float: left
+    }
+    .authority h5 {
+        margin-top: -10px;
+        color: #037c09;
+        /*text-align: center;*/
+        margin-left: 35px;
+    }
+
+    .authority1 h5 {
+        margin-top: -10px;
+        color: #037c09;
+        /*text-align: center;*/
+        margin-left: 35px;
+    }
+    
+    .thanks p {
+        color: #136108;;
+        font-size: 16px;
+        font-weight: normal;
+        font-family: serif;
+        margin-top: 20px;
+    }
+
+    .t {
+  border: 1px solid black;
+  border-collapse: collapse;
+}
+
+</style>
+
+</head>
+<body>
+
+  <table width="100%" style="background: #f7f7f7; padding:0 0px 0 0px;">
+    <tr>
+        <td valign="top">
+          <br>   
+          <img width="200px" height="72px" src="{{ asset('/public/frontend/assets/img/logo2.png') }}" alt="">
+        </td>
+        <td align="right">
+          <pre class="font" style="margin: 2px; line-height: 1;">
+            STATA IT LIMITED 
+            Email: statabangladesh@gmail.com
+            {{-- <br> --}}
+            Mob: 88 09678200509 
+          </pre>
+          <h3>Date : <span> {{$sdate}}</span> - <span>{{$edate}} </span></h3>
+        </td>
+    </tr>
+  </table>
+
+
+  <table width="100%" style="background:white; padding:2px;"></table>
+  
+  <br/>
+{{-- <h3>Product List</h3> --}}
+  <table class="t" width="100%">
+    <thead style="background-color: #17810e; color:#FFFFFF;">
+      <tr class="font">
+        <th class="t">SL.</th>
+        <th class="t">Date</th>
+        <th class="t">Purchase Number</th>
+        <th class="t">Supplier</th>
+        <th class="t">Amount</th>
+        
+      </tr>
+    </thead>
+    <tbody>
+        @php
+            $sl = 1;
+          $amount = 0;
+        @endphp
+     @foreach($filter as $purchase)
+    
+      <tr class="font">
+        <td class="t" align="center">{{$sl++}}</td>
+        <td class="t" align="center">{{$purchase->purchase_date}}</td>
+        <td class="t" align="center">{{$purchase->chalan_no}}</td>
+        <td class="t" align="center">{{$purchase->supplier->supplier_name}} </td>
+        <td class="t" align="center">{{$purchase->grand_total}}</td>
+        <td class="t" align="center" style="display:none;">{{$amount += $purchase->grand_total}}</td>
+      </tr>
+      @endforeach
+      <tr>
+        <td class="t" align="center"></td>
+        <td class="t" align="center"></td>
+        <td class="t" align="center"></td>
+        <td class="t" align="center"></td>						
+        <td class="t" align="center">{{$amount}}</td>	   
+       </tr>
+    </tbody>
+  </table>
+  
+</body>
+</html> 
