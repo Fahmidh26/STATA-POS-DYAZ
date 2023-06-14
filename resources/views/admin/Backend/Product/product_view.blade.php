63ccbd00366023795bd1ed0a5edd2d3eb88031b7
ifazalam1 <ifaz.alam1@gmail.com>
2023-06-14 12:32:14 +0600

    cost price hidden

--- resources/views/admin/Backend/Product/product_view.blade.php
+++ resources/views/admin/Backend/Product/product_view.blade.php
@@ -16,9 +16,11 @@
 									<thead>
 										<tr class="align-middle text-center">
 											<th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Image</th>
-											<th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Code</th>
 											<th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Name</th>
+											<th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Code</th>
+											@if(Auth::guard('admin')->user()->type=="1" || (Auth::guard('admin')->user()->type=="2"))
 											<th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Cost Price</th>
+											@endif
 											<th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Sale Price</th>
 											<th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Stock</th>
 											<th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Action</th>
@@ -39,12 +41,13 @@
 					<td><img src="{{ asset($item->product_img) }}" style="width: 70px; height: 40px;"> </td>
 					<td><p class="mb-0 text-sm">{{ $item->product_name }}</p></td>
 					<td><h6 class="mb-0 text-sm">{{ $item->product_code }}</h6></td>
+					@if(Auth::guard('admin')->user()->type=="1" || (Auth::guard('admin')->user()->type=="2"))
 					<td><h6 class="mb-0 text-sm">{{ $item->cost_price }}</h6></td>
+					@endif
 					<td><h6 class="mb-0 text-sm">{{ $item->sale_price }}</h6></td>
 					<td><h6 class="mb-0 text-sm">{{ $item->qty }}</h6></td>
 					<td hidden><h6 >{{ $tSale += $item->sale_price * $item->qty}}</h6></td>
 					<td hidden><h6 >{{ $tCost += $item->cost_price * $item->qty}}</h6></td>
-					
 					<td>
 
 			 <a class="btn btn-link text-dark px-3 mb-0" href="{{ route('product.edit',$item->id) }}"><i class="fas fa-pencil-alt text-dark me-2" aria-hidden="true"></i>Edit</a>
