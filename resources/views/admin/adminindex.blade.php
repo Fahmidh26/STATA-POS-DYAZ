d5fc35c0c0be01873e4dab84a43868ee776c4051
Fahmid <fahmidh26@gmail.com>
2023-06-18 12:13:30 +0600

    Index Fahmid

--- resources/views/admin/adminindex.blade.php
+++ resources/views/admin/adminindex.blade.php
@@ -329,6 +329,7 @@
 </div>
 	
 	  </div>
+	  @if(Auth::guard('admin')->user()->type=="1" || (Auth::guard('admin')->user()->type=="2"))
 	  <div class="col-lg-4 col-md-6">
 		<div class="card h-100">
 		  <div class="card-header pb-0">
@@ -379,7 +380,7 @@
 		  </div> --}}
 		</div>
 	  </div>
-	 
+	  @endif
 	</div>
 
 	@include('admin.body.footer')
