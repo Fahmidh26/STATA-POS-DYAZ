7b18f8623c22a70a8b9beff7ad1c3f5931e85525
ifazalam1 <ifaz.alam1@gmail.com>
2023-07-10 13:08:18 +0600

    product delete

--- resources/views/admin/Backend/Product/product_view.blade.php
+++ resources/views/admin/Backend/Product/product_view.blade.php
@@ -57,7 +57,9 @@
 					@if(Auth::guard('admin')->user()->type=="1" || (Auth::guard('admin')->user()->type=="2"))
 					<td>
 			 <a class="btn btn-link text-dark px-3 mb-0" href="{{ route('product.edit',$item->id) }}"><i class="fas fa-pencil-alt text-dark me-2" aria-hidden="true"></i>Edit</a>		
-			 {{-- <a class="btn btn-link text-danger text-gradient px-3 mb-0" href="{{ route('product.delete',$item->id) }}"><i class="far fa-trash-alt me-2" aria-hidden="true"></i>Delete</a> --}}
+			 @if(Auth::guard('admin')->user()->type=="1" || (Auth::guard('admin')->user()->type=="2"))
+			 <a class="btn btn-link text-danger text-gradient px-3 mb-0" href="{{ route('product.delete',$item->id) }}"><i class="far fa-trash-alt me-2" aria-hidden="true"></i>Delete</a>
+			 @endif
 					</td>
 					@endif
 										 
